6ed3c3b9c9a33824e0d6041a8da677b6180c7fe4
Bhar32arya <bhar32arya@gmail.com>
2024-01-13 18:42:45 +0530

    renamed file

--- src/main/java/org/example/SingletonEnumClass.java
+++ src/main/java/org/example/SingletonEnumClass.java
@@ -1,5 +1,13 @@
 package org.example;
 
+/*Thread Safety: Enums are inherently thread-safe,
+and the singleton instance is created only once during class loading.
+
+Serialization: Enum instances are inherently serializable,
+and their deserialization is guaranteed to produce the same instance.
+
+Reflection: Enum types cannot be instantiated using reflection,
+preventing instantiation bypassing the singleton pattern*/
 enum SingletonEnum {
     INSTANCE;
     // Additional methods and properties can be added here
@@ -8,7 +16,7 @@ enum SingletonEnum {
     }
 }
 
-public class SingletonMainClass {
+public class SingletonEnumClass {
     public static void main(String[] args) {
         SingletonEnum anEnum = SingletonEnum.INSTANCE;
         SingletonEnum anEnum1 = SingletonEnum.INSTANCE;
