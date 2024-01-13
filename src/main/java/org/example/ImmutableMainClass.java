fc7c3a92f07238bc79a7ce012cb6038f50ae0f15
Bhar32arya <bhar32arya@gmail.com>
2024-01-13 19:06:11 +0530

    added immutable class example

---
+++ src/main/java/org/example/ImmutableMainClass.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+final class Immutable {
+    private final int rollNumber;
+    private final String name;
+    private final List<String> address;
+
+    Immutable(int rollNumber, String name, List<String> addressList) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.address = new ArrayList<>(addressList);
+    }
+
+    public  final int getRollNumber() {
+        return this.rollNumber;
+    }
+    public final String getName() {
+        return this.name;
+    }
+
+    public final List<String> getAddress() {
+        return Collections.unmodifiableList(this.address);
+    }
+}
+public class ImmutableMainClass {
+    public static void main(String[] args) {
+        Immutable immutable = new Immutable(123, "bhar32", Arrays.asList("Purnia", "Bangalore"));
+    }
+}
